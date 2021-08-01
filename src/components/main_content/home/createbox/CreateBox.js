6fd147901d364020b58cda58bc6cdda1a5964810
Karan Purohit <karnpurohit@gmail.com>
2021-08-01 11:26:31 +0000

    Added join button

--- src/components/main_content/home/createbox/CreateBox.js
+++ src/components/main_content/home/createbox/CreateBox.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import './CreateBox.css'
 
 export default class CreateBox extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //   }
+
+    joinHandler = ()=>{
+        console.log("Join called");
+    }
+
     render() {
         return (
             <div className="create_box box">
@@ -13,7 +22,14 @@ export default class CreateBox extends Component {
 
                     <input type="text" name="playerName" placeholder="Your Name" className="dice_input" />
                     
-                    <input type="button" value="Submit" />
+                    <select defaultValue="3" name="diceNum" className="dice_input_selection" >
+                        <option value="1">Number of dice: 1</option>
+                        <option value="2">Number of dice: 2</option>
+                        <option value="3">Number of dice: 3</option>
+                    </select>
+
+                    <input type="button" value="Join" className="dice_input_button" onClick={this.joinHandler}/>
+
                 </form>
             </div>
         )
