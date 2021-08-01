c82416379f0a8bae87454be2322fb121358c7875
Karan Purohit <karnpurohit@gmail.com>
2021-08-01 10:27:01 +0000

    Form styling

--- src/components/main_content/home/createbox/CreateBox.js
+++ src/components/main_content/home/createbox/CreateBox.js
@@ -8,6 +8,13 @@ export default class CreateBox extends Component {
                 <div className="create_title">
                     Create Session
                 </div>
+                <form className="create_form">
+                    <input type="text" name="gameName" placeholder="Name of the Game" className="dice_input" />
+
+                    <input type="text" name="playerName" placeholder="Your Name" className="dice_input" />
+                    
+                    <input type="button" value="Submit" />
+                </form>
             </div>
         )
     }
